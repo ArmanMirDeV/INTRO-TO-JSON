6f12b84ae6e3e14742837d72f822fcf526790643
Arman Mir <mirarman8583@gmail.com>
2025-09-06 10:43:51 +0600

    loadPost from url

--- introJSON.js
+++ introJSON.js
@@ -1,8 +1,17 @@
-
+// fetch gives promise of response... 
 
 const loadData= ()=>{
 fetch("https://jsonplaceholder.typicode.com/todos/1").then((res) => res.json()).then((data) => console.log(data));
 }
+
+
+const loadPost = () =>{
+
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    fetch(url).then(res=> res.json()).then(json => console.log(json) )
+
+    }
+
 
 
 /* 
